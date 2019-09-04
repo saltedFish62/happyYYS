d72d6a7b045ec7c7acab36d39c14b990625b386e
SaltedFish <470963817@qq.com>
2019-09-04 21:57:56 +0800

    update

--- utils.py
+++ utils.py
@@ -8,6 +8,9 @@ import pyautogui
 import numpy as np
 import time
 import cv2
+import os
+
+import matplotlib.pyplot as plt
 
 
 # search for the windows of yys
@@ -63,16 +66,26 @@ def capture(win):
 
 
 # 模板匹配查找
-def find(win, path):
-    img = capture(win)
-    templLoc = pyautogui.locateAll(haystackImage=img, needleImage=path, grayscale=True, confidence=0.7)
-    return list(templLoc)
+def find(templ, image, win=0):
+    if win != 0:
+        img = capture(win)
+    else:
+        img = image
+    
+    templW, templH = templ.shape[::-1]
+    print(templW, templH)
+    matchResult = cv2.matchTemplate(img, templ, cv2.TM_CCOEFF_NORMED)
+
+    locs = np.where(matchResult >= 0.85)
+    result = []
+    for pt in zip(*locs[::-1]):
+        result.append((pt[0], pt[1], templW, templH))
+    return result
 
 
 # 是否存在
-def has(win, path):
-    return len(find(win=win, path=path)) != 0
-
+def has(templ, win=0, image=[]):
+    return len(find(win=win, image=image, templ=templ)) != 0
 
 # 休眠一段时间
 def sleep(min, max):
@@ -91,11 +104,20 @@ def cropImg(img, topLeft, bottomRight):
     bottom = bottomRight[1] if bottomRight[1] < h else h-1
     return img[int(top) : int(bottom), int(left) : int(right)]
 
+# 灰度读取图片
+def readImg(path):
+    return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+# 加载图片
+def loadImages():
+    images = {}
+    path = os.getcwd() + '/images'
+    imgList = os.listdir(path)
+
+    for img in imgList:
+        name = img.split('.')[0]
+        images[name] = readImg(path+'/'+img)
+    return images
 
 if __name__ == "__main__":
-    wins = search()
-    i = 0
-    for win in wins:
-        setPos(win, 600 * i, 0, 600, 0)
-        i+=1
-    pass
+    print(loadImages()['captain'])
