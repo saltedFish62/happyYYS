8ed3d68ae032cf6dc634105093ed4405d634271a
saltedFish <chenyx_62@163.com>
2019-09-21 12:04:07 +0800

    update

--- yuling.py
+++ yuling.py
@@ -1,7 +1,7 @@
 from utils import *
 
 win = 0
-win = search()
+win = search()[0]
 
 if win == 0:
   print('no onmychi')
@@ -13,13 +13,13 @@ images = loadImages()
 
 while True:
   img = capture(win)
-  locs = find(image=img, templ=images[''])
+  locs = find(image=img, templ=images['yuling_entry'])
   if len(locs) > 0:
     clickRange(win, locs[0])
-  locs1 = find(image=img, templ=images[''])
+  locs1 = find(image=img, templ=images['vic1'])
   if len(locs1) > 0:
     clickRange(win, locs1[0])
-  locs2 = find(image=img, templ=images[''])
+  locs2 = find(image=img, templ=images['vic2'])
   if len(locs2) > 0:
     clickRange(win, locs2[0])
   sleep(1, 2)
